4890548d32fd059a795bdd91fee93fdf2fe37b7f
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-04-07 19:48:36 +0300

    healthy face

---
+++ src/components/page/main/Header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//  COMPONENTS
+
+//  STYLE
+import style from './header'
+
+const header = () => {
+    return (
+        <div className={style.header}>
+            hello
+        </div>
+    );
+};
+
+export default header;
---
+++ src/components/page/main/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//  COMPONENTS
+import Header from './Header/header.jsx'
+
+//  STYLES
+
+
+const main = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default main;
