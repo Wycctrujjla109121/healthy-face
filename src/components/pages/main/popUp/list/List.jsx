7857a4ad40b87c5a399d439ece9e94e1de82aa18
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-04-21 03:48:16 +0300

    fixed popup

--- src/components/pages/main/popUp/list/List.jsx
+++ src/components/pages/main/popUp/list/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 //  STYLES
 import style from './list.module.scss'
@@ -11,6 +12,7 @@ const List = (setActive) => {
                 <a className={style.list__info__link} href="#">Спреи</a>
                 <a className={style.list__info__link} href="#">Заказать</a>
                 <a className={style.list__info__link} href="#">Обратная связь</a>
+                <NavLink className={style.list__info__text} to='/cart'>Корзина (4)</NavLink>
                 <p className={style.list__info__phone}>+7 999-999-99-99</p>
             </div>
         </div>
