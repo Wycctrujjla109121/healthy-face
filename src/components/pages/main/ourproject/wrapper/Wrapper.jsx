5497836401baf819e0213854e2a71a34fb4e697a
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-05-03 17:22:06 +0300

    add accordeon in ourpoject

--- src/components/pages/main/ourproject/wrapper/Wrapper.jsx
+++ src/components/pages/main/ourproject/wrapper/Wrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //  Btn IMG
 import btnLeft from './svg/btnLeft.svg'
@@ -14,24 +14,101 @@ import Product from './product/Product.jsx'
 //  STYLES
 import style from './wrapper.module.scss'
 
-const wrapper = () => {
+const Wrapper = () => {
+
+    const [product, setProduct] = useState([
+        {
+            priceNew: '1499 р.',
+            priceOld: '2199 р.',
+            name: 'Phyther',
+            img: { img0 }
+        },
+        {
+            priceNew: '999 р.',
+            priceOld: '1299 р.',
+            name: 'Derma',
+            img: { img1 }
+        },
+        {
+            priceNew: '1199 р.',
+            priceOld: '1599 р.',
+            name: 'Eco-phyther',
+            img: { img2 }
+        },
+        {
+            priceNew: '1299 р.',
+            priceOld: '1599 р.',
+            name: 'Farma',
+            img: { img3 }
+        },
+        {
+            priceNew: '799 р.',
+            priceOld: '1199 р.',
+            name: 'Consular',
+            img: { img4 }
+        }
+    ])
+
+    const [offset, setOffset] = useState(0)
+
+    const resultProduct = Array.from(product.map((product, text) => {
+        return <Product priceNew={product.priceNew} priceOld={product.priceOld} name={product.name} img={product.img} key={text} offset={offset} />
+    }))
+
+    // Переменные для offset
+    const maxWidthProduct = 264;
+
+
+    function handleLeftClick() {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset + maxWidthProduct
+            console.log(currentOffset)
+
+            return Math.min(newOffset, 0)
+        })
+    }
+
+    function handleRightCLick() {
+        let windowScreen = window.innerWidth;
+        let maxWidthList = 0;
+        // Максимальная длинна листа 
+        if (windowScreen > 1550) {
+            maxWidthList = 1320;
+        }
+        else if (windowScreen < 1550 && windowScreen > 1420) {
+            maxWidthList = 1206;
+        }
+        else{
+            maxWidthList = windowScreen - 214
+        }
+
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset - maxWidthProduct
+
+
+            const maxOffset = -(maxWidthProduct * (product.length)) + maxWidthList
+            console.log('Текущий offset: ' + currentOffset)
+            console.log('Максимальная offset: ' + maxOffset)
+
+            return Math.max(newOffset, maxOffset)
+        })
+    }
+
     return (
         <div className={style.wrapper}>
-                <button className={style.wrapper__button}>
-                    <img className={style.wrapper__img} src={btnLeft} alt="" />
-                </button>
-                <div className={style.wrapper__list}>
-                    <Product priceNew = '1499 р.' priceOld = '2199 р.' name = 'Phyther' img={img0} />
-                    <Product priceNew = '999 р.' priceOld = '1299 р.' name = 'Derma' img={img1} />
-                    <Product priceNew = '1199 р.' priceOld = '1599 р.' name = 'Eco-phyther' img={img2}/>
-                    <Product priceNew = '1299 р.' priceOld = '1599 р.' name = 'Farma' img={img3}/>
-                    <Product priceNew = '799 р.' priceOld = '1199 р.' name = 'Consular' img={img4}/>
-                </div>
-                <button className={style.wrapper__button}>
-                    <img className={style.wrapper__img} src={btnRight} alt="" />
-                </button>
+            <button className={style.wrapper__button} onClick={handleLeftClick}>
+                <img className={style.wrapper__img} src={btnLeft} alt="" />
+            </button>
+            <div className={style.wrapper__list}>
+
+                {resultProduct}
+
             </div>
+            <button className={style.wrapper__button}>
+                <img className={style.wrapper__img} src={btnRight} alt="" onClick={handleRightCLick} />
+            </button>
+        </div>
     );
 };
 
-export default wrapper;
+export default Wrapper;
--- src/components/pages/main/ourproject/wrapper/product/Product.jsx
+++ src/components/pages/main/ourproject/wrapper/product/Product.jsx
@@ -5,7 +5,11 @@ import style from './product.module.scss'
 
 const product = (props) => {
     return (
-        <div className={style.product} style={ {backgroundImage: `url(${props.img})`} }>
+        <div className={style.product} style={
+            {
+                backgroundImage: `url(${props.img})`,
+                transform: `translateX(${props.offset}px)`
+            }}>
             <div className={style.product__info}>
                 <div className={style.product__price}>
                     <h3 className={style.product__priceNew}>{props.priceNew}</h3>
